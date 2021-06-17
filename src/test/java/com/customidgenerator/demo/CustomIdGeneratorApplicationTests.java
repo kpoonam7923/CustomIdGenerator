12a3044a2492a290e4d6e5f50db377aeacd3f36a
Poonam Arunrao Kale <poonam.arunrao.kale@ibm.com>
2021-06-17 15:39:24 +0530

    initial commit

---
+++ src/test/java/com/customidgenerator/demo/CustomIdGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customidgenerator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomIdGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
